cd87e72c07f2ab19699ca62dd7e677f38224f14a
nelezaha <albert.zaha@mayermobile.at>
2015-12-05 21:54:50 +0100

    First change

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Nele!");
+		System.out.println("Hello Hana!");
+	}
+
+}
